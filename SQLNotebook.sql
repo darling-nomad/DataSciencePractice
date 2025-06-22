e9e9935613e3fd50e5b13431c34cd690ae172fd0
darling-nomad <github@darlingnomad.me>
2025-06-21 20:24:09 -0400

    Initial commit: add PythonPractice.ipynb, SQLNotebook.sql, and notes.ipynb

---
+++ SQLNotebook.sql
@@ -0,0 +1,42 @@
+/*markdown
+# SQL Practice Notebook
+#### Table of Contents
+**[Interview Master →](#interview-master)** <br>
+**[HackerRank →](#hackerrank)**
+*/
+
+/*markdown
+### Interview Master
+*/
+
+/*markdown
+##### The problem
+*/
+
+-- The code
+
+/*markdown
+The takeaway
+*/
+
+/*markdown
+### HackerRank
+*/
+
+/*markdown
+##### The problem
+*/
+
+-- The code
+
+/*markdown
+The takeaway
+*/
+
+
+
+/*markdown
+#### Jump To
+**[Interview Master →](#interview-master)** <br>
+**[HackerRank →](#hackerrank)**
+*/
